29c16ee3cc697d985380cb9d9cc324d9b5bd175e
tama <repeatedly@gmail.com>
2008-09-13 02:49:39 +0900

    Implement rand_regex method and this test

--- lib/strrand.rb
+++ lib/strrand.rb
@@ -1,4 +1,4 @@
-# -*- mode: ruby; encoding: utf-8 -*-
+# -*- mode: ruby; coding: utf-8 -*-
 
 # = strrand.rb: Generates a random string from a pattern
 #
@@ -8,26 +8,26 @@
 # See http://search.cpan.org/~steve/String-Random-0.22/
 
 class StringRandom
-  UPPER  = ('A'..'Z').to_a
-  LOWER  = ('a'..'z').to_a
-  DIGIT  = (0..9).to_a
-  PUNCT  = [33..47, 58..64, 91..96, 123..126].map do |range|
+  Upper  = ('A'..'Z').to_a
+  Lower  = ('a'..'z').to_a
+  Digit  = ('0'..'9').to_a
+  Punct  = [33..47, 58..64, 91..96, 123..126].map do |range|
     range.map { |val| val.chr }
   end.flatten
-  ANY    = UPPER + LOWER + DIGIT + PUNCT
-  SALT   = UPPER + LOWER + DIGIT + ['.', '/']
-  BINARY = (0..255).map { |val| val.chr }
+  Any    = Upper | Lower | Digit | Punct
+  Salt   = Upper | Lower | Digit | ['.', '/']
+  Binary = (0..255).map { |val| val.chr }
 
   # These are the regex-based patterns.
-  PATTERN = {
+  Pattern = {
     # These are the regex-equivalents.
-    '.'  => ANY,
-    '\d' => DIGIT,
-    '\D' => UPPER + LOWER + PUNCT,
-    '\w' => UPPER + LOWER + DIGIT + ['_'],
-    '\W' => PUNCT.reject { |val| val == '_' },
-    '\s' => [' ', '\t'],
-    '\S' => UPPER + LOWER + DIGIT + PUNCT,
+    '.'  => Any,
+    '\d' => Digit,
+    '\D' => Upper | Lower | Punct,
+    '\w' => Upper | Lower | Digit | ['_'],
+    '\W' => Punct.reject { |val| val == '_' },
+    '\s' => [' ', "\t"],
+    '\S' => Upper | Lower | Digit | Punct,
 
     # These are translated to their double quoted equivalents.
     '\t' => ["\t"],
@@ -40,65 +40,154 @@ class StringRandom
   # What's important is how they relate to the pattern characters.
   # These are the old patterns for rand_pattern.
   OldPattern = {
-    'C' => UPPER,
-    'c' => LOWER,
-    'n' => DIGIT,
-    '!' => PUNCT,
-    '.' => ANY,
-    's' => SALT,
-    'b' => BINARY
+    'C' => Upper,
+    'c' => Lower,
+    'n' => Digit,
+    '!' => Punct,
+    '.' => Any,
+    's' => Salt,
+    'b' => Binary
   }
   # These characters are treated specially in randregex.
   Regch = {
     "\\" => lambda { |ch, chars, string|
-      unless chars.empty?
-        tmp = chars.shift
-        if tmp = 'x'
-          # This is supposed to be a number in hex, so
-          # there had better be at least 2 characters left.
-          tmp = chars.shift + chars.shift
-          string << tmp.hex.chr
-        elsif tmp =~ /[0-7]/
-          warn "octal parsing not implemented.  treating literally."
-          string << tmp
-        elsif Pattern["\\#{tmp}"]
-          ch     << tmp
-          string << ch
-        else
-          warn "'\\#{tmp}' being treated as literal '#{tmp}'"
-          string << tmp
-        end
+      raise "regex not terminated" if chars.empty?
+
+      tmp = chars.shift
+      if tmp == 'x'
+        # This is supposed to be a number in hex, so
+        # there had better be at least 2 characters left.
+        tmp = chars.shift + chars.shift
+        string << tmp.hex.chr
+      elsif tmp =~ /[0-7]/
+        warn "octal parsing not implemented.  treating literally."
+        string << tmp
+      elsif Pattern.has_key?(ch + tmp)
+        string << Pattern[ch + tmp]
       else
-        raise "regex not terminated"
+        warn "'\\#{tmp}' being treated as literal '#{tmp}'"
+        string << tmp
       end
     },
-    '.'  => lambda { |ch, chars, string|
+    '.' => lambda { |ch, chars, string|
       string << Pattern[ch]
     },
-    '['  => lambda { |ch, chars, string|
+    '[' => lambda { |ch, chars, string|
+      tmp = []
+      while ch = chars.shift and ch != ']'
+        if ch == '-' and !chars.empty? and !tmp.empty?
+          ch  = chars.shift
+          num = tmp.last[0]
+          max = ch[0]
+          while num < max
+            num += 1
+            tmp << num.chr
+          end
+        else
+          warn "${ch}' will be treated literally inside []" if ch =~ /\W/
+          tmp << ch
+        end
+      end
+
+      raise "unmatched []" if ch != ']'
+      string << tmp
     },
-    '*'  => lambda { |ch, chars, string|
+    '*' => lambda { |ch, chars, string|
       chars.unshift("{0,}".split(""))
     },
-    '+'  => lambda { |ch, chars, string|
+    '+' => lambda { |ch, chars, string|
       chars.unshift("{1,}".split(""))
     },
-    '?'  => lambda { |ch, chars, string|
+    '?' => lambda { |ch, chars, string|
       chars.unshift("{0,1}".split(""))
     },
-    '{'  => lambda { |ch, chars, string|
+    '{' => lambda { |ch, chars, string|
+      # { isn't closed, so treat it literally.
+      return string << ch unless chars.include?('}')
+
+      tmp = ''
+      while ch = chars.shift and ch != '}'
+        raise "'#{ch}' inside {} not supported" unless (ch =~ /[\d,]/)
+        tmp << ch
+      end
+
+      if tmp =~ /,/
+        if tmp =~ /^(\d*),(\d*)$/
+          min = $1.length.zero? ? 0    : $1.to_i
+          max = $2.length.zero? ? @max : $2.to_i
+          raise "bad range {#{tmp}}" if min > max
+
+          tmp = (min == max ? min : min + rand(max - min + 1))
+          #if min == max
+          #  tmp = min
+          #else
+          #  tmp = min + rand(max - min + 1)
+          #end
+        else
+          raise "malformed range {#{tmp}}"
+        end
+      else
+        tmp = tmp.to_i
+      end
+
+      unless tmp.zero?
+        last = string.last
+        (tmp - 1).times do
+          string << last
+        end
+      else
+        string.pop
+      end
+      }
     }
-  }
 
   def initialize(max = 10)
     @max = max
+    @old = OldPattern.clone
   end
 
-  def rand_regex
+  def rand_regex(patterns)
+    return _rand_regex(patterns) unless patterns.instance_of?(Array)
+
+    results = []
+    patterns.each do |pattern|
+      results << _rand_regex(pattern)
+    end
+    results
+  end
+
+  def [](key)
+    @old[key]
+  end
+
+  def []=(key, val)
+    @old[key] = val
+  end
+
+  def rand_pattern(pattern)
 
   end
 
-  def rand_pattern
+  private
 
+  def _rand_regex(pattern)
+    string = []
+    regch  = Regch
+    chars  = pattern.split(//)
+
+    while ch = chars.shift
+      if regch.has_key?(ch)
+        regch[ch].call(ch, chars, string)
+      else
+        warn "'#{ch}' not implemented. treating literally." if ch =~ /[\$\^\*\(\)\+\{\}\]\|\?]/
+        string << ch
+      end
+    end
+
+    result = ''
+    string.each do |ch|
+      result << ch[rand(ch.size)]
+    end
+    result
   end
 end
--- test/test_strrand.rb
+++ test/test_strrand.rb
@@ -2,14 +2,56 @@
 # test/test_strrand.rb
 #
 
+$KCODE      = 'u'
 $LOAD_PATH << '../'
 
 require 'lib/strrand'
 require 'test/unit'
 
 class TestStringRandom < Test::Unit::TestCase
-  def test_rand_regex
+  def setup
+    @string_random = StringRandom.new
+  end
 
+  def test_initialize
+    assert_not_nil(@string_random)
+    assert_instance_of(StringRandom, @string_random)
+  end
+
+  def test_method_respond
+    assert_respond_to(@string_random, :[])
+    assert_respond_to(@string_random, :[]=)
+    assert_respond_to(@string_random, :rand_regex)
+    assert_respond_to(@string_random, :rand_pattern)
+  end
+
+  def test_rand_regex
+    patterns = ['\d\d\d',
+                '\w\w\w',
+                '[ABC][abc]',
+                '[012][345]',
+                '...',
+                '[a-z][0-9]',
+                '[aw-zX][123]',
+                '[a-z]{5}',
+                '0{80}',
+                '[a-f][nprt]\d{3}',
+                '\t\n\r\f\a\e',
+                '\S\S\S',
+                '\s\s\s',
+                '\w{5,10}',
+                '\w?',
+                '\w+',
+                '\w*',
+                '']
+
+    patterns.each do |pattern|
+      result = @string_random.rand_regex(pattern)
+      assert_match(/#{pattern}/, result)
+    end
+
+    result = @string_random.rand_regex(patterns)
+    assert_equal(result.size, patterns.size)
   end
 
   def test_rand_pattern
