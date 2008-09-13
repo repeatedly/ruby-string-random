bcbf505ad3246eeef609ebb6a4b5ffdb063afbe0
tama <repeatedly@gmail.com>
2008-09-13 22:02:34 +0900

    Change Regch implementation, Const to instance variable

--- lib/strrand.rb
+++ lib/strrand.rb
@@ -48,102 +48,19 @@ class StringRandom
     's' => Salt,
     'b' => Binary
   }
-  # These characters are treated specially in randregex.
-  Regch = {
-    "\\" => lambda { |ch, chars, string|
-      raise "regex not terminated" if chars.empty?
-
-      tmp = chars.shift
-      if tmp == 'x'
-        # This is supposed to be a number in hex, so
-        # there had better be at least 2 characters left.
-        tmp = chars.shift + chars.shift
-        string << tmp.hex.chr
-      elsif tmp =~ /[0-7]/
-        warn "octal parsing not implemented.  treating literally."
-        string << tmp
-      elsif Pattern.has_key?(ch + tmp)
-        string << Pattern[ch + tmp]
-      else
-        warn "'\\#{tmp}' being treated as literal '#{tmp}'"
-        string << tmp
-      end
-    },
-    '.' => lambda { |ch, chars, string|
-      string << Pattern[ch]
-    },
-    '[' => lambda { |ch, chars, string|
-      tmp = []
-      while ch = chars.shift and ch != ']'
-        if ch == '-' and !chars.empty? and !tmp.empty?
-          ch  = chars.shift
-          num = tmp.last[0]
-          max = ch[0]
-          while num < max
-            num += 1
-            tmp << num.chr
-          end
-        else
-          warn "${ch}' will be treated literally inside []" if ch =~ /\W/
-          tmp << ch
-        end
-      end
-
-      raise "unmatched []" if ch != ']'
-      string << tmp
-    },
-    '*' => lambda { |ch, chars, string|
-      chars.unshift("{0,}".split(""))
-    },
-    '+' => lambda { |ch, chars, string|
-      chars.unshift("{1,}".split(""))
-    },
-    '?' => lambda { |ch, chars, string|
-      chars.unshift("{0,1}".split(""))
-    },
-    '{' => lambda { |ch, chars, string|
-      # { isn't closed, so treat it literally.
-      return string << ch unless chars.include?('}')
-
-      tmp = ''
-      while ch = chars.shift and ch != '}'
-        raise "'#{ch}' inside {} not supported" unless (ch =~ /[\d,]/)
-        tmp << ch
-      end
-
-      if tmp =~ /,/
-        if tmp =~ /^(\d*),(\d*)$/
-          min = $1.length.zero? ? 0    : $1.to_i
-          max = $2.length.zero? ? @max : $2.to_i
-          raise "bad range {#{tmp}}" if min > max
-
-          tmp = (min == max ? min : min + rand(max - min + 1))
-          #if min == max
-          #  tmp = min
-          #else
-          #  tmp = min + rand(max - min + 1)
-          #end
-        else
-          raise "malformed range {#{tmp}}"
-        end
-      else
-        tmp = tmp.to_i
-      end
-
-      unless tmp.zero?
-        last = string.last
-        (tmp - 1).times do
-          string << last
-        end
-      else
-        string.pop
-      end
-      }
-    }
 
   def initialize(max = 10)
-    @max = max
-    @old = OldPattern.clone
+    @max   = max
+    @old   = OldPattern.clone
+    @regch = {
+      "\\" => method(:regch_slash),
+      '.'  => method(:regch_dot),
+      '['  => method(:regch_bracket),
+      '*'  => method(:regch_asterisk),
+      '+'  => method(:regch_plus),
+      '?'  => method(:regch_question),
+      '{'  => method(:regch_brace)
+    }
   end
 
   def rand_regex(patterns)
@@ -172,14 +89,14 @@ class StringRandom
 
   def _rand_regex(pattern)
     string = []
-    regch  = Regch
     chars  = pattern.split(//)
+    non_ch = /[\$\^\*\(\)\+\{\}\]\|\?]/  # not supported chars
 
     while ch = chars.shift
-      if regch.has_key?(ch)
-        regch[ch].call(ch, chars, string)
+      if @regch.has_key?(ch)
+        @regch[ch].call(ch, chars, string)
       else
-        warn "'#{ch}' not implemented. treating literally." if ch =~ /[\$\^\*\(\)\+\{\}\]\|\?]/
+        warn "'#{ch}' not implemented. treating literally." if ch =~ non_ch
         string << ch
       end
     end
@@ -189,5 +106,98 @@ class StringRandom
       result << ch[rand(ch.size)]
     end
     result
+  end
+
+  # These characters are treated specially in randregex.
+  def regch_slash(ch, chars, string)
+    raise "regex not terminated" if chars.empty?
+
+    tmp = chars.shift
+    if tmp == 'x'
+      # This is supposed to be a number in hex, so
+      # there had better be at least 2 characters left.
+      tmp = chars.shift + chars.shift
+      string << tmp.hex.chr
+    elsif tmp =~ /[0-7]/
+      warn "octal parsing not implemented. treating literally."
+      string << tmp
+    elsif Pattern.has_key?(ch + tmp)
+      string << Pattern[ch + tmp]
+    else
+      warn "'\\#{tmp}' being treated as literal '#{tmp}'"
+      string << tmp
+    end
+  end
+
+  def regch_dot(ch, chars, string)
+    string << Pattern[ch]
+  end
+
+  def regch_bracket(ch, chars, string)
+    tmp = []
+    while ch = chars.shift and ch != ']'
+      if ch == '-' and !chars.empty? and !tmp.empty?
+        ch  = chars.shift
+        num = tmp.last[0]
+        max = ch[0]
+        while num < max
+          num += 1
+          tmp << num.chr
+        end
+      else
+        warn "${ch}' will be treated literally inside []" if ch =~ /\W/
+        tmp << ch
+      end
+    end
+
+    raise "unmatched []" if ch != ']'
+    string << tmp
+  end
+
+  def regch_asterisk(ch, chars, string)
+    chars.unshift("{0,}".split(""))
+  end
+
+  def regch_plus(ch, chars, string)
+    chars.unshift("{1,}".split(""))
+  end
+
+  def regch_question(ch, chars, string)
+    chars.unshift("{0,1}".split(""))
+  end
+
+  def regch_brace(ch, chars, string)
+    # { isn't closed, so treat it literally.
+    return string << ch unless chars.include?('}')
+
+    tmp = ''
+    while ch = chars.shift and ch != '}'
+      raise "'#{ch}' inside {} not supported" unless ch =~ /[\d,]/
+      tmp << ch
+    end
+
+    if tmp =~ /,/
+      raise "malformed range {#{tmp}}" unless tmp =~ /^(\d*),(\d*)$/
+
+      p local_variables
+      p self.instance_variable_get("@max")
+      min = $1.length.nonzero? ? $1.to_i : 0
+      max = $2.length.nonzero? ? $2.to_i : @max
+      puts "#{min.class} : #{max.class}"
+      raise "bad range {#{tmp}}" if min > max
+
+      tmp = (min == max ? min : min + rand(max - min + 1))
+    else
+      tmp = tmp.to_i
+    end
+
+    if tmp.nonzero?
+      last = string.last
+      (tmp - 1).times do
+        string << last
+      end
+    else
+      string.pop
+    end
   end
 end
